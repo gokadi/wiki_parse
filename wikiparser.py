28b348a7a349d8f8eadbf5e68ca5f9297e7566c0
gokadi <gokadi@yandex.ru>
2017-08-23 00:24:57 +0300

    Initial commit

---
+++ wikiparser.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import wikipedia
+from bs4 import BeautifulSoup
+# FIXME: add arg to filter 'See also', 'Notes', 'References', 'External links'
+article_name = "Michael Jackson"  # input() or "Michael Jackson"  # Uncomment in production
+article = wikipedia.page(article_name)
+# In case of multiple possible articles upper line throws 'wikipedia.DisambiguationError'
+wiki_sect_subsect = [BeautifulSoup(s, "html.parser").get_text() for s in article.sections]
+soup = BeautifulSoup(article.html(), "html.parser")
+get_section = lambda h: h.get_text().replace('[edit]', '')
+# We need below to differ sections from subsections
+soup_sect = [get_section(h2) for h2 in soup.findAll('h2')
+             if get_section(h2) != 'Contents']
+soup_subsect = [get_section(h3) for h3 in soup.findAll('h3')
+                if get_section(h3) != 'Contents']
+
+p_article = list()
+for sect_name in wiki_sect_subsect:
+    ins_list = list()
+    ins_list.append(sect_name)
+    ins_list.append(article.section(sect_name).replace("\\'", "'"))  # FIXME: doesn't replacing
+    if sect_name in soup_sect:
+        ins_list.append('Section')
+    elif sect_name in soup_subsect:
+        ins_list.append('Subsection')
+    else:
+        ins_list.append('ERROR. No section %s' % sect_name)
+    ins_list.append('Summarization')
+    ins_list.append('Keywords')
+    p_article.append(ins_list)
+with open('C:/first_try.txt', 'w', encoding='utf-8') as file:
+    file.write(str(p_article))
+print(p_article)
